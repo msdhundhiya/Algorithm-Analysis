0ec436db9fb2d62e2cfa2a699e16714e77f6d9e9
msdhundhiya <78727981+msdhundhiya@users.noreply.github.com.>
2025-08-04 08:59:20 +0530

    fix: Add basename to BrowserRouter for GH Pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './assets/styles.css';
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Algorithm-Analysis/">
     <App />
     </BrowserRouter>
     
